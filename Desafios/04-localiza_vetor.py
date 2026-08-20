4dbf8f630d1f0e294beb8cd8c02cd095493938a3
inaciofragalli <inaciofragali@gmail.com>
2026-08-20 15:05:38 -0300

    feat: adição do menu

--- Desafios/04-localiza_vetor.py
+++ Desafios/04-localiza_vetor.py
@@ -1,45 +1,71 @@
 from random import randint
 
 def gerarVetor(n: int) -> list[int]:
-    return [ randint(0,10) for _ in range(n) ]
+    return [ randint(0, 10) for _ in range(n) ]
 
-def procurarVetor(vetorMaior: list[int], procurado: list[int]) -> int:
-    if (len(procurado) > len(vetorMaior)):
-        return -1
+def localizarVetor(vetorMaior: list[int], vetorMenor: list[int]) -> list[int]:
+    indices: list[int] = []
 
-    indiceMenor: int = -1
+    if len(vetorMaior) < len(vetorMenor):
+        print('Vetores com tamanhos incompatíveis. Vetor gerado maior que vetor procurado.')
+        return []
 
     for i in range(len(vetorMaior)):
-
-        if vetorMaior[i] == procurado[0]:
-            contador: int = 0
-            temp: int = i
+        if vetorMaior[i] == vetorMenor[0]:
+            contador: int = 1
+            temp: int = i + 1
             indiceEntrado: int = i
 
-            for j in range(1, len(procurado)):
-                if temp + 1 < len(vetorMaior):
-                    if vetorMaior[temp+1] == procurado[j]:
+            if len(vetorMenor) > 1:
+                for j in range(1, len(vetorMenor)):
+                    
+                    if vetorMaior[temp] == vetorMenor[j]:
                         contador += 1
+                        temp += 1
 
-            if contador == len(procurado) - 1:
-                indiceMenor = indiceEntrado
+                    if contador == len(vetorMenor):
+                        indices.append(indiceEntrado)
+            else:
+                indices.append(indiceEntrado)
 
+    return indices
 
-    return indiceMenor
+if __name__ == '__main__':
+    valido: bool = True
 
+    padraoN: int = 25
+    padraoM: int = 2
 
-if __name__ == "__main__":
-    n: int = int(input("Digite a quantidade de números: "))
+    while valido:
+        print(f'(1) configuração de n e m (padrões: {padraoN}, {padraoM})\n' + \
+                '(2) localizar todas as ocorrências do vetor menor\n' + \
+                '(3) remover todas as ocorrências do vetor menor\n' + \
+                '(4) substituir todas as ocorrências do vetor menor\n' + \
+                '(5) finalizar programa\n')
 
-    vetorMaior: list[int] = gerarVetor(n)
-    print(vetorMaior)
+        opcao: int = int(input('Digite uma opção: '))
+        print()
 
-    procurado: list[int] = []
-    vetorMenor = input("Digite o vetor menor (entre espaços): ")
+        if opcao == 1:
+            novoN: int = int(input(f'Digite o valor de n (padrão = {padraoN}): '))
+            novoM: int = int(input(f'Digite o valor de m (padrão = {padraoM}): '))
+            print()
 
-    for i in vetorMenor.split():
-        procurado.append(int(i))
+            padraoN = novoN
+            padraoM = novoM
 
-    print(procurarVetor(vetorMaior, procurado))
+        elif opcao == 2:
+            vetorMaior: list[int] = gerarVetor(padraoN)
+            print(f'Vetor maior: {vetorMaior}')
 
-    
+            vetorMenor: list[int] = []
+
+            for i in range(padraoM):
+                valor: int = int(input(f'Valor {i + 1} do vetor menor: '))
+                vetorMenor.append(valor)
+
+            print(localizarVetor(vetorMaior, vetorMenor), '\n')
+
+        elif opcao == 5:
+            valido = False
+            print('Programa finalizado')
