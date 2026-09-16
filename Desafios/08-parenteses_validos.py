b05796faae8d9f4875347f1f84f3f7ca6208ac62
inaciofragalli <inaciofragalli@gmail.com>
2026-09-16 15:31:37 -0300

    parenteses validos

--- Desafios/08-parenteses_validos.py
+++ Desafios/08-parenteses_validos.py
@@ -1,27 +1,32 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from copy import deepcopy
 
+
 TERRA: int = -1
 
+
 @dataclass
-class TipoItem:
+class tipoItem:
     valor: int | float | str | None = None
 
-class Pilha:
+class PilhaEstatica:
     def __init__(self, tamMax: int):
-        self._pilha: list[TipoItem] = [ TipoItem() for i in range(tamMax) ]
+        self._pilha: list[tipoItem] = [
+            tipoItem() for _ in range(tamMax)
+        ]
         self._tamMax: int = tamMax
         self._topo: int = TERRA
-        self.rem: TipoItem = TipoItem()
+        self.item: tipoItem | None = None
 
-    def estaVazia(self) -> bool:
+    def vazia(self) -> bool:
         return self._topo == TERRA
 
-    def estaCheia(self) -> bool:
+    def cheia(self) -> bool:
         return self._topo == self._tamMax - 1
 
-    def empilhar(self, item: TipoItem) -> bool:
-        if self.estaCheia():
+    def empilha(self, item: tipoItem) -> bool:
+        if self.cheia():
             return False
 
         self._topo += 1
@@ -29,65 +34,130 @@ class Pilha:
 
         return True
 
-    def desempilhar(self) -> bool:
-        if self.estaVazia():
+    def desempilha(self) -> bool:
+        if self.vazia():
+            self.item = None
             return False
 
-        self.rem = deepcopy(self._pilha[self._topo])
+        self.item = deepcopy(self._pilha[self._topo])
         self._topo -= 1
+
         return True
 
-
-    def topo(self) -> TipoItem:
-        if self.estaVazia():
-            return TipoItem()
+    def topo(self) -> tipoItem | None:
+        if self.vazia():
+            return None
 
         return deepcopy(self._pilha[self._topo])
 
-    def mostrar(self) -> None:
-        if self.estaVazia():
-            print('\nSituação da Pilha: vazia\n')
+    def mostra(self, texto: str) -> None:
+        if self.vazia():
+            print(f"\nSituação da Pilha {texto}: Vazia!")
 
         else:
-            print('\nSituação da Pilha:')
+            print(f"\nSituação da Pilha {texto}:")
 
-            for i in range(self._topo + 1, -1, -1):
+            for i in range(self._topo, -1, -1):
                 print(self._pilha[i].valor)
 
-    def esvaziar(self) -> None:
-        self._topo = -1
-        self.rem = TipoItem()
+    def esvazia(self) -> None:
+        self._topo = TERRA
+        self.item = None
 
-def ehValido(s: str) -> bool:
-    pilha: Pilha = Pilha(len(s))
-    
+@dataclass
+class _tipoNo:
+    item: tipoItem | None = None
+    _prox: _tipoNo | None = None
+
+
+class PilhaDinamica:
+    def __init__(self, maxTam: int | None = None):
+        self._topo: _tipoNo | None = None
+        self.item: tipoItem | None = None
+
+    def vazia(self) -> bool:
+        return self._topo is None
+
+    def cheia(self) -> bool:
+        return False
+
+    def empilha(self, item: tipoItem) -> bool:
+        novoNo = _tipoNo()
+
+        novoNo.item = deepcopy(item)
+        novoNo._prox = self._topo
+
+        self._topo = novoNo
+
+        return True
+
+    def desempilha(self) -> bool:
+        if self.vazia():
+            self.item = None
+            return False
+
+        self.item = deepcopy(self._topo.item)
+
+        aux = self._topo
+        self._topo = self._topo._prox
+        aux._prox = None
+
+        return True
+
+    def topo(self) -> tipoItem | None:
+        if self.vazia():
+            return None
+
+        return deepcopy(self._topo.item)
+
+    def mostra(self, texto: str) -> None:
+        if self.vazia():
+            print(f"\nSituação da Pilha {texto}: Vazia!")
+
+        else:
+            print(f"\nSituação da Pilha {texto}:")
+
+            pos = self._topo
+
+            while pos is not None:
+                print(pos.item.valor)
+                pos = pos._prox
+
+    def esvazia(self) -> None:
+        self.item = None
+        self._topo = None
+
+
+def ehValido(s: str, TipoPilha) -> bool:
+    pilha = TipoPilha(len(s))
+
     for i in range(len(s)):
-        paren: str = s[i]
-    
-        if paren == '(' or paren == '[' or paren == '{':
-            pilha.empilhar(paren)
+        caractere: str = s[i]
 
-        if paren == ')' or paren == ']' or paren == '}':
-            if pilha.estaVazia():
+        if caractere == '(':
+            pilha.empilha(tipoItem(caractere))
+
+        elif caractere == ')':
+            if pilha.vazia():
+                print("Expressão incorreta: falta abre parêntese.")
                 return False
 
-            pilha.desempilhar()
-            topo: str = pilha.rem
+            pilha.desempilha()
 
-            if paren == ')' and topo != '(':
-                return False
+    if not pilha.vazia():
+        print("Expressão incorreta: falta fecha parêntese.")
+        return False
 
-            elif paren == ']' and topo != '[':
-                return False
-
-            elif paren == '}' and topo != '{':
-                return False
-
-
-    return pilha.estaVazia()
+    print("Expressão correta.")
+    return True
 
 
 if __name__ == '__main__':
-    s: str = input('Digite a sequência de parênteses: ')
+    expressao: str = input('Digite uma expressão matemática: ')
+    tipoPilha: int = int(input('Escolha o tipo da pilha: \n1) Estática\n2) Dinâmica\n'))
 
-    print(ehValido(s))
+    match tipoPilha:
+        case 1:
+            ehValido(expressao, PilhaEstatica)
+        case 2:
+            ehValido(expressao, PilhaDinamica)
